d6ff329d59e670172b24c7abe92e61cb4fb43442
Ben Cail <bcail@crossway.org>
2024-08-23 17:22:27 -0400

    update folder handling - fetch from server if needed, and display from storage

--- email_client.py
+++ email_client.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from contextlib import contextmanager
 import json
 import os
 import sqlite3
@@ -30,7 +31,7 @@ class EmailServer:
         }
         r = self._post_request(request)
         method_responses = r.json()['methodResponses']
-        return [{'id': m['id'], 'name': m['name']} for m in method_responses[0][1]['list']]
+        return [{'id': m['id'], 'name': m['name'], 'role': m['role'], 'parent_id': m['parentId']} for m in method_responses[0][1]['list']]
 
     def get_emails(self, folder_id, limit=10):
         method_calls = [
@@ -102,14 +103,29 @@ class EmailServer:
         return list(method_responses[0][1]['list'][0]['bodyValues'].values())[0]['value']
 
 
+@contextmanager
+def sqlite_txn(cursor):
+    cursor.execute('BEGIN IMMEDIATE')
+    try:
+        yield
+        cursor.execute('COMMIT')
+    except BaseException as e:
+        cursor.execute('ROLLBACK')
+        raise
+
+
 class Storage:
     DB_INIT_STATEMENTS = [
-        'CREATE TABLE folder ('
+        'CREATE TABLE folders ('
             'id INTEGER PRIMARY KEY,'
-            'server_id TEXT NOT NULL,'
+            'server_id TEXT NOT NULL UNIQUE,'
             'name TEXT NOT NULL,'
-            'CHECK (server_id != "")'
-            'CHECK (name != "")'
+            'parent_server_id TEXT NULL,'
+            'role TEXT NULL,'
+            'CHECK (server_id != ""),'
+            'CHECK (name != ""),'
+            'CHECK (parent_server_id != ""),'
+            'FOREIGN KEY(parent_server_id) REFERENCES folders(server_id)'
             ') STRICT',
         'CREATE TABLE emails ('
             'id INTEGER PRIMARY KEY,'
@@ -133,30 +149,11 @@ class Storage:
         conn.execute('PRAGMA foreign_keys = ON;')
         return conn
 
-    @staticmethod
-    def _begin_txn(cursor):
-        cursor.execute('BEGIN IMMEDIATE')
-
-    @staticmethod
-    def _rollback(cursor):
-        try:
-            cursor.execute('ROLLBACK')
-        except sqlite3.OperationalError as e:
-            if str(e) == 'cannot rollback - no transaction is active':
-                pass
-            else:
-                raise
-
     def _create_tables(self):
         cursor = self._conn.cursor()
-        Storage._begin_txn(cursor)
-        try:
+        with sqlite_txn(cursor):
             for statement in Storage.DB_INIT_STATEMENTS:
                 cursor.execute(statement)
-            cursor.execute('COMMIT')
-        except:
-            Storage._rollback(cursor)
-            raise
 
     def __init__(self, db_name):
         # creates & initializes database if needed
@@ -165,20 +162,47 @@ class Storage:
         if not tables:
             self._create_tables()
 
+    @property
+    def has_folders(self):
+        result = self._conn.execute('SELECT COUNT(*) FROM folders').fetchone()
+        if int(result[0]) > 0:
+            return True
+
+    def save_folder(self, folder_id, name, role, parent_id):
+        cursor = self._conn.cursor()
+        with sqlite_txn(cursor):
+            cursor.execute('INSERT INTO folders(server_id, name, role, parent_server_id) VALUES(?, ?, ?, ?)', (folder_id, name, role, parent_id))
+
+    def get_folders(self):
+        results = self._conn.execute('SELECT server_id,name FROM folders ORDER BY name').fetchall()
+        return [{'id': r[0], 'name': r[1]} for r in results]
+
 
 if __name__ == '__main__':
     import requests
     import markdownify
 
+    email_file = os.environ['EMAIL_FILE']
+
+    storage = Storage(email_file)
     server = EmailServer()
-    folders = server.get_folders()
-    print(folders)
-    emails = server.get_emails(folders[0]['id'])
-    for index, email in enumerate(emails):
-        print(f'{index} -- {email}')
-    while True:
-        response = input('select email (q to quit): ')
-        if response.lower() in ['q', '']:
-            break
-        html_data = server.get_email_html_data(emails[int(response)]['id'])
-        print(markdownify.markdownify(html_data))
+
+    if not storage.has_folders:
+        print(f'Fetching folders...')
+        folders = server.get_folders()
+        for f in folders:
+            storage.save_folder(folder_id=f['id'], name=f['name'], role=f['role'], parent_id=f['parent_id'])
+
+    folders = storage.get_folders()
+    for f in folders:
+        print(f'{f["id"]} -- {f["name"]}')
+
+    # emails = server.get_emails(folders[0]['id'])
+    # for index, email in enumerate(emails):
+    #     print(f'{index} -- {email}')
+    # while True:
+    #     response = input('select email (q to quit): ')
+    #     if response.lower() in ['q', '']:
+    #         break
+    #     html_data = server.get_email_html_data(emails[int(response)]['id'])
+    #     print(markdownify.markdownify(html_data))
